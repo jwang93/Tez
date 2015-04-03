5cdecbc9921f859d1eede583e32ce4af438d6a87
Jay Wang <jmw86@duke.edu>
2015-04-03 01:10:30 -0400

    Refactor benchmark structure.
    Added /execution_plan that details the execution plan for each MR or Tez job
    Added /queries directory within each benchmark for easy access to the queries

--- hive-0.13.1/benchmarks/benchmark 1/mr_workload.sh
+++ hive-0.13.1/benchmarks/benchmark 1/mr_workload.sh
@@ -1,6 +1,5 @@
 echo 'Benchmark 1 - MR workload'
 echo 'running overweight_zips_mr.sql script in /queries'
-cd ../..
 echo 'run 1'
 bin/hive -f queries/overweight_zips_mr.sql 
 echo 'run 2'
--- hive-0.13.1/benchmarks/benchmark 1/tez_workload.sh
+++ hive-0.13.1/benchmarks/benchmark 1/tez_workload.sh
@@ -1,6 +1,5 @@
 echo 'Benchmark 1 - Tez workload'
 echo 'running overweight_zips_tez.sql script in /queries'
-cd ../..
 echo 'run 1'
 bin/hive -f queries/overweight_zips_tez.sql 
 echo 'run 2'
