11367b511358f02c623c9d9d12076244dcd0f827
Jay Wang <jmw86@duke.edu>
2015-04-09 12:50:38 -0400

    Fixing errors in the benchmark scripts.
    - moved queries into the /hive dir
    - added a guide for error handling
    - detailed solution to error when namenode does not start

--- benchmarks/benchmark 1/tez_workload.sh
+++ benchmarks/benchmark 1/tez_workload.sh
@@ -2,11 +2,14 @@ echo 'Benchmark 1 - Tez workload'
 echo 'running overweight_zips_tez.sql script in /queries'
 cd ../../hive-0.13.1
 echo 'setting up tables'
-bin/hive -f queries/create_tables.sql
+bin/hive -f queries/benchmark\ 1/create_tables.sql
 echo 'run 1'
-bin/hive -f queries/overweight_zips_tez.sql 
+bin/hive -f queries/benchmark\ 1/overweight_zips_tez.sql 
 echo 'run 2'
-bin/hive -f queries/overweight_zips_tez.sql 
+bin/hive -f queries/benchmark\ 1/overweight_zips_tez.sql 
 echo 'run 3'
-bin/hive -f queries/overweight_zips_tez.sql 
+bin/hive -f queries/benchmark\ 1/overweight_zips_tez.sql 
 echo 'Benchmark 1 - Tez complete'
+
+
+
