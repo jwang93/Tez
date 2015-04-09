577f14184075aea13f5bfb39c407fb013bc8037a
Jay Wang <jmw86@duke.edu>
2015-04-09 13:25:56 -0400

    adding benchmark 2

---
+++ benchmarks/benchmark 2/mr_workload.sh
@@ -0,0 +1,8 @@
+echo 'Benchmark 2 - MR workload'
+echo 'running overweight_zips_mr.sql script in /queries'
+cd ../../hive-0.13.1
+echo 'setting up tables'
+bin/hive -f queries/benchmark\ 1/create_tables.sql
+echo '-- running ---'
+bin/hive -f queries/benchmark\ 1/overweight_zips_mr.sql 
+echo 'Benchmark 2 - MR complete'
---
+++ benchmarks/benchmark 2/queries/explain_overweight_zips_mr.sql
@@ -0,0 +1,13 @@
+set hive.execution.engine = mr;
+
+EXPLAIN
+SELECT 'Average population density per zip code: ', AVG(pop_density_sq_mi) AS avg_pop_density_sq_mi 
+FROM population_density_zip;
+
+EXPLAIN
+SELECT 'Average population density per zip code (top overweight): ', AVG(pop_density_sq_mi) AS avg_pop_density_sq_mi 
+FROM top_overweight_zips;
+
+EXPLAIN
+SELECT 'Average population density per zip code (least overweight): ', AVG(pop_density_sq_mi) AS avg_pop_density_sq_mi 
+FROM least_overweight_zips;
