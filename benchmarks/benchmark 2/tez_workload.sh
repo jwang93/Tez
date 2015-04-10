b31566d912b5576fa4c431be36ccb8c05178c180
Jay Wang <jmw86@duke.edu>
2015-04-10 02:57:09 -0400

    Added input data for running the benchmarks

--- benchmarks/benchmark 2/tez_workload.sh
+++ benchmarks/benchmark 2/tez_workload.sh
@@ -2,9 +2,9 @@ echo 'Benchmark 2 - Tez workload'
 echo 'running overweight_zips_tez.sql script in /queries'
 cd ../../hive-0.13.1
 echo 'setting up tables'
-bin/hive -f queries/benchmark\ 1/create_tables.sql
+bin/hive -f queries/benchmark\ 2/create_tables.sql
 echo '-- running ---'
-bin/hive -f queries/benchmark\ 1/overweight_zips_tez.sql 
+bin/hive -f queries/benchmark\ 2/overweight_zips_tez.sql 
 echo 'Benchmark 2 - Tez complete'
 
 
