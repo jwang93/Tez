24791385c37daf81f51cea8c14a929efad64e1d3
Jay Wang <jmw86@duke.edu>
2015-04-12 22:55:33 -0400

    added the results / logs for benchmark 3

--- benchmarks/benchmark 3/mr_workload.sh
+++ benchmarks/benchmark 3/mr_workload.sh
@@ -1,8 +1,8 @@
-echo 'Benchmark 2 - MR workload'
-echo 'running overweight_zips_mr.sql script in /queries'
+echo 'Benchmark 3 - MR workload'
+echo 'running income_mr.sql script in /queries'
 cd ../../hive-0.13.1
 echo 'setting up tables'
-bin/hive -f queries/benchmark\ 2/create_tables.sql
+bin/hive -f queries/benchmark\ 3/create_tables.sql
 echo '-- running ---'
-bin/hive -f queries/benchmark\ 2/overweight_zips_mr.sql 
-echo 'Benchmark 2 - MR complete'
+bin/hive -f queries/benchmark\ 3/income_mr.sql 
+echo 'Benchmark 3 - MR complete'
--- benchmarks/benchmark 3/tez_workload.sh
+++ benchmarks/benchmark 3/tez_workload.sh
@@ -1,11 +1,11 @@
-echo 'Benchmark 2 - Tez workload'
-echo 'running overweight_zips_tez.sql script in /queries'
+echo 'Benchmark 3 - Tez workload'
+echo 'running income_tez.sql script in /queries'
 cd ../../hive-0.13.1
 echo 'setting up tables'
-bin/hive -f queries/benchmark\ 2/create_tables.sql
+bin/hive -f queries/benchmark\ 3/create_tables.sql
 echo '-- running ---'
-bin/hive -f queries/benchmark\ 2/overweight_zips_tez.sql 
-echo 'Benchmark 2 - Tez complete'
+bin/hive -f queries/benchmark\ 3/income_tez.sql 
+echo 'Benchmark 3 - Tez complete'
 
 
 
